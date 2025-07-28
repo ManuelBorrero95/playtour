98ba1544fadd2ca7670ddf46163a037e1fec85e3
Manuel Borrero <borreromanuel95@gmail.com>
2025-07-28 12:24:01 +0200

    add tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,26 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import CreateEvent from './pages/CreateEvent';
+import Home from './pages/Home';
+
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/WeatherForecast`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Errore nella risposta del server');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log("Dati ricevuti:", data);
-        setData(data);
-      })
-      .catch(error => {
-        console.error("Errore nella fetch:", error);
-      });
-  }, []);
-
   return (
-    <div>
-      <h1>Previsioni Meteo</h1>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>
-            {item.date} - {item.summary} - {item.temperatureC}°C
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+           <Route path="/crear-evento" element={<CreateEvent />} />
+        </Routes>
+      </Layout>
+    </Router>
+
+
+
+
+  )
 }
 
 export default App;
